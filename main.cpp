db17a6be8db80e9ad53fbcd893120e97b390d720
claranceliberi <liberintwari@gmail.com>
2022-07-01 14:55:12 +0200

    feat: seetings

--- main.cpp
+++ main.cpp
@@ -248,3 +248,138 @@ void Shopping::edit(){
     }
 
 }
+
+void Shopping::rem(){
+    fstream data, data1;
+
+    int pkey;
+    int token=0;
+
+    cout << "\n\t\t\t Delete product ";
+    cout << "\n\t\t\t Product code :";
+    cin >> pkey;
+    data.open("database.txt", ios::in);
+
+    if(!data){
+        cout << "\n\nFile doesn't exists! ";
+    }else {
+        data1.open("database.txt", ios::app | ios::in);
+        data >> pcode >> pname >> price >> discount;
+        while(!data.eof()){
+            if(pkey == pcode){
+                cout << "\n\n\t Product deleted successfully";
+            } else {
+                data1 << " " << pcode << " " << pname << " " << price << " " << discount << "\n";
+            }
+
+            data >> pcode >> pname >> price >> discount;
+        }
+
+        data.close();
+        data1.close();
+
+        remove("database.txt");
+        rename("database1.txt","database.txt");
+
+        if(token == 0){
+            cout << "\n\n Record not found sorry";
+        }
+
+    }
+}
+
+
+void Shopping :: list(){
+    fstream data;
+    data.open("database.txt", ios::in);
+
+    cout << "\n\n|___________________________________________________________________________|";
+    cout << "\n\n|ProNo\t\tName\t\tPrice\n";
+    cout << "\n\n|___________________________________________________________________________|";
+    data >> pcode >> pname >> price >> discount;
+    while(!data.eof()){
+        cout << pcode << "\t\t" << pname << "\t\t" << price << "\n";
+        data >> pcode >> pname >> price >> discount;
+    }
+
+    data.close();
+    
+}
+
+void Shopping :: receipt(){
+    m:
+    fstream data;
+    int arrc[100];
+    int arrq[100];
+
+    char choice;
+    int c=0;
+    float ammount =0;
+    float dis =0;
+    float total = 0;
+
+
+    cout << "\n\n\t\t\t\t   RECEIPT";
+    data.open("database.txt",ios::in);
+
+    if(!data){
+        cout << "\n\n   Empty database" ;
+    }else {
+        data.close();
+        list();
+        
+        cout << "\n\n|_________________________________________________________________|";
+        cout << "\n\n|                                                                 |";
+        cout << "\n\n|            Please place the order                               |";
+        cout << "\n\n|                                                                 |";
+        cout << "\n\n|_________________________________________________________________|";
+
+        do{
+            cout << "\n\nEnter Product code";
+            cin >> arrc[c];
+            cout << "\n\nEnter the product quantity";
+            cin >> arrq[c];
+            
+            for(int i=0; i<c; i++){
+                if(arrc[c] == arrc[i]){
+                    cout << "\n\nDuplicated product code\n";
+                    goto m;
+                }
+            }
+
+            c++;
+
+            cout << "\n\nDo you want to buy another product? if yes press y else no";
+            cin >> choice;
+
+        }while(choice == 'y');
+
+        cout << "\n\n\t\t\t______________________________RECEIPT____________________\n";
+        cout << "\nProduct No \t Product name \t Product quantity \t price \t Ammount \t Ammount with discout \n";
+
+        for(int i=0; i<c; i++){
+            data.open("database.txt", ios::in);
+            data >> pcode >> pname >> price >> discount;
+            
+            while(!data.eof()){
+                if(pcode == arrc[i]){
+                    ammount = price*arrq[i];
+                    dis = ammount - (ammount * discount/100);
+                    total = total + dis;
+                    cout << "\n" << pcode << " \t " << pname << " \t " << arrq[i]  << " \t " << price << " \t " << ammount << " \t "  << dis;
+                }
+
+                data >> pcode >> pname >> price >> discount;
+            }
+        }
+
+        data.close();
+
+    }
+
+    cout << "\n\n_________________________________________________________\n";
+    cout << "\n\nTotal Ammount : \n\n";
+
+
+
+}
