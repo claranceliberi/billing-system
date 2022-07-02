41b3f71abf821735afe3eb3507d591af795fe4c5
claranceliberi <liberintwari@gmail.com>
2022-07-02 22:22:18 +0200

    feat: select all from file

--- csv.cpp
+++ csv.cpp
@@ -2,29 +2,69 @@
 #include <fstream>
 #include <vector>
 #include <sstream>
+#include <map>
+#include <iterator>
 
 using namespace std;
 
+class CSV{
+    private:
+        const char *filename;
 
-int main(){
+    public:
+        CSV(const char *filename);
+        map<string, vector<string>> selectAll();
+};
 
-    fstream file("data.csv");
+CSV :: CSV(const char *filename){
+    this->filename = filename;
+}
 
+map<string, vector<string>> CSV::selectAll(){
+    fstream file(this->filename);
     string line;
 
+    map<string, vector<string>> dataSet;
     vector<string> row;
     string word;
 
     while(getline(file,line)){
+        row.clear();
         stringstream s(line);
 
         while (getline(s, word, ',')) {
-            cout << word << "\t\t" ;
-            
+            row.push_back(word);
         }
+
+        dataSet.insert({row[0], row});
 
         cout << endl;
     }
+
+    return dataSet;
+}
+
+
+void displayVector(vector<string> _vect){
+    copy(_vect.begin(), _vect.end(),ostream_iterator<string>(cout, " "));
+}
+
+int main(){
+
+    CSV csv("data.csv");
+
+    
+    map<string, vector<string>> list = csv.selectAll();
+
+    map<string,vector<string> >::iterator it;
+
+    for (it = list.begin(); it != list.end(); it++)
+    {
+        cout << it->first << "\t\t";
+        displayVector(it->second);
+        cout << endl;
+    }
+
 
     return 0;
 }
